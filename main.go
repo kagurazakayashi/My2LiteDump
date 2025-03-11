b019d608a312a8855af456f3fc85c9afccf6b891
KagurazakaMiyabi (神楽坂雅詩) <miyabi@uuu.moe>
2025-03-11 20:18:15 +0800

    连接到MySQL和SQLite

--- main.go
+++ main.go
@@ -1,89 +1,121 @@
 package main
 
 import (
+	"encoding/json"
 	"flag"
-	"fmt"
 	"log"
-	"strconv"
+	"os"
+	"path/filepath"
 	"strings"
-	"syscall"
 
 	nyamysql "github.com/kagurazakayashi/libNyaruko_Go/nyamysql"
-	"golang.org/x/term"
+	nyasqlite "github.com/kagurazakayashi/libNyaruko_Go/nyasqlite"
+	"gopkg.in/yaml.v3"
 )
 
-var NullStr string
+var (
+	NullStr string
+	mysql   *nyamysql.NyaMySQL
+	sqlite  *nyasqlite.NyaSQLite
+)
+
+type My2LiteConfig struct {
+	MySQLTable string `json:"mysql_table" yaml:"mysql_table"`
+}
 
 func main() {
-	// 定義命令列引數: MySQL
-	var host *string = flag.String("h", "127.0.0.1", "MySQL 服务器地址")
-	var port *int = flag.Int("P", 3306, "MySQL 端口号")
-	var dbname *string = flag.String("D", "", "MySQL 数据库名称")
-	var mytable *string = flag.String("T", "", "MySQL 表名")
-	var user *string = flag.String("u", "root", "MySQL 用户名")
-	var password *string = flag.String("p", "", "MySQL 密码")
-	var exKey *string = flag.String("K", "id", "要导出内容的 key")
-	var exVal *string = flag.String("V", "", "要导出的内容区间（整数-整数）")
-	// 定義命令列引數: SQLite
-	var dbfile *string = flag.String("f", "", "SQLite 数据库文件路径")
-	var litetable *string = flag.String("t", "", "SQLite 表名")
+	log.Println("My2LiteDump v1.0.0")
+	var configContent string
+	var contentBytes []byte
+	var my2LiteConfig My2LiteConfig
+	var errInfo string = ""
+
+	var configFile *string = flag.String("c", "", "配置文件路径,默认则用`程序名.yaml||.json`")
 	flag.Parse()
 
-	// _, err := os.Stat(*dbfile)
-	// if os.IsNotExist(err) {
-	// 	log.Fatalf("SQLite 数据库文件 %s 不存在!\n", *dbfile)
-	// 	return
-	// }
-
-	// 如果 -p 選項提供但沒有指定密碼，則要求使用者輸入密碼（隱藏輸入）
-	if len(*password) == 0 {
-		fmt.Print("请输入 MySQL 密码 (不回显): ")
-		bytePassword, err := term.ReadPassword(int(syscall.Stdin))
+	if len(strings.TrimSpace(*configFile)) == 0 {
+		execPath, err := os.Executable()
 		if err != nil {
-			log.Fatalf("读取密码失败: %v", err)
+			log.Println("获取执行文件路径失败。")
+			log.Fatal(err)
 		}
-		fmt.Println()
-		*password = string(bytePassword)
-	}
 
-	// 構造 MySQL 連線字串
-	var dsn string
-	if *dbname != "" {
-		dsn = fmt.Sprintf("%s:%s@tcp(%s:%d)/%s", *user, *password, *host, *port, *dbname)
+		execName := filepath.Base(execPath)
+		baseName := execName[:len(execName)-len(filepath.Ext(execName))]
+
+		dir := filepath.Dir(execPath)
+		yamlPath := filepath.Join(dir, baseName+".yaml")
+		jsonPath := filepath.Join(dir, baseName+".json")
+
+		log.Println("正在加载配置文件: " + yamlPath)
+		if bytes, err := os.ReadFile(yamlPath); err == nil {
+			contentBytes = bytes
+		} else {
+			errInfo = err.Error()
+		}
+		if len(errInfo) > 0 {
+			log.Println("正在加载配置文件: " + jsonPath)
+			if bytes, err := os.ReadFile(jsonPath); err == nil {
+				contentBytes = bytes
+				errInfo = ""
+			} else {
+				errInfo += "\n" + err.Error()
+			}
+		}
+		if len(errInfo) > 0 {
+			log.Println("配置文件读取失败。")
+			log.Fatal(errInfo)
+		}
 	} else {
-		dsn = fmt.Sprintf("%s:%s@tcp(%s:%d)/", *user, *password, *host, *port)
+		log.Println("正在加载配置文件: " + *configFile)
+		if bytes, err := os.ReadFile(*configFile); err == nil {
+			contentBytes = bytes
+		} else {
+			log.Println("配置文件读取失败：" + *configFile)
+			log.Fatal(err)
+		}
 	}
-	fmt.Println("正在连接到数据库:", MaskPassword(dsn))
+	configContent = string(contentBytes)
+	if err := json.Unmarshal(contentBytes, &my2LiteConfig); err != nil {
+		errInfo = err.Error()
+	}
+	if len(errInfo) > 0 {
+		if err := yaml.Unmarshal(contentBytes, &my2LiteConfig); err != nil {
+			errInfo += err.Error()
+		} else {
+			errInfo = ""
+		}
+	}
+	if len(errInfo) > 0 {
+		log.Println("配置文件解析失败。")
+		log.Fatal(errInfo)
+	}
+	log.Println("成功。")
 
-	var vall []string = strings.Split(*exVal, "-")
+	logger := log.New(os.Stdout, "[MySQLDump] ", log.Ldate|log.Ltime|log.Lshortfile)
 
-	startValue, err := strconv.ParseInt(vall[0], 10, 64)
-	if err != nil {
-		log.Fatalf("无效的 StartValue: %v", err)
+	log.Println("初始化 MySQL ...")
+	mysql = nyamysql.New(configContent, logger)
+	if mysql.Error() != nil {
+		log.Println("失败！")
+		log.Fatal(mysql.Error())
 	}
-	endValue, err := strconv.ParseInt(vall[1], 10, 64)
-	if err != nil {
-		log.Fatalf("无效的 EndValue: %v", err)
+	log.Println("成功。")
+	log.Println("初始化 SQLite ...")
+	sqlite = nyasqlite.New(configContent, logger)
+	if sqlite.Error() != nil {
+		log.Println("失败！")
+		disconnect()
+		log.Fatal(sqlite.Error())
 	}
-	config := ExportConfig{
-		MySQLDSN:    dsn,
-		SQLiteFile:  *dbfile,
-		MySQLTable:  *mytable,
-		SQLiteTable: *litetable,
-		KeyColumn:   *exKey,
-		StartValue:  startValue,
-		EndValue:    endValue,
-		BatchSize:   50,
-	}
-	mysqlConfig := nyamysql.MySQLDBConfig{
-		User:     *user,
-		Password: *password,
-		Address:  *host,
-		Port:     strconv.Itoa(*port),
-		DbName:   *dbname,
-		MaxLimit: "10000",
-	}
-	if err := BatchExportMySQLToSQLite(config, mysqlConfig); err != nil {
-		return
-	}
+	log.Println("成功。")
+	disconnect()
+	log.Println("正常退出。")
+}
+
+func disconnect() {
+	log.Println("断开 MySQL 。")
+	mysql.Close()
+	log.Println("断开 SQLite 。")
+	sqlite.Close()
 }
